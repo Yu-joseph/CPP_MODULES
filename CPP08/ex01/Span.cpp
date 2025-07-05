05214ba4173d8c9030bf77af54a1e206af8fe48c
You-Mega <youssefsouhail1997@gmail.com>
2025-07-05 23:45:00 +0200

    funct shortlist

--- CPP08/ex01/Span.cpp
+++ CPP08/ex01/Span.cpp
@@ -1,7 +1,48 @@
 #include "Span.hpp"
 
-Span::Span(unsigned int N) : max_size(N){}
+Span::Span(unsigned int N) : N(N){}
 
-void Span::addNumber
+
+Span::Span( void ) : N(0) { }
+
+
+Span::Span( const Span& ls ) : N(ls.N), sp(ls.sp) { }
+
+Span& Span::operator=( const Span& ls ) {
+	if (this != &ls) {
+		this->N = ls.N;
+		this->sp = ls.sp;
+	}
+	return *this;
+}
+
+void Span::addNumber(unsigned int nb)
+{
+	if (this->sp.size() >= N)
+	{
+		throw std::out_of_range(std::string("there is no space"));
+	}
+	this->sp.push_back(nb);
+	
+}
+
+int Span::shortestSpan()
+{
+	if (sp.size() < 2)
+	{
+		throw std::invalid_argument(std::string("should size be more than 1"));
+	}
+	else{
+		std::vector<int> tmp(sp);
+    	std::sort(tmp.begin(), tmp.end());
+		int minspan = std::numeric_limits<int>::max();
+		for (size_t i = 0; i < tmp.size()-1 ; i++)
+		{
+			/* code */
+		}
+		
+	}
+	
+}
 
 Span::~Span(){}
--- CPP08/ex01/Span.hpp
+++ CPP08/ex01/Span.hpp
@@ -2,15 +2,21 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <limits>
 
 class Span
 {
 private:
-	unsigned int max_size;
-	std::vector<int> numb;
+	unsigned int N; //max int
+	std::vector<int> sp;
 public:
 	Span(unsigned int N);
+	Span(void);
+	Span& operator=( const Span& ls );
+	Span( const Span& ls );
 	void addNumber(unsigned int x);
+	int shortestSpan();
 	~Span();
 };
 
