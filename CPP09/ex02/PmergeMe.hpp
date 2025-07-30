d78685d40e9ff1ea348b97a32275c36516f8e7f9
Youssef Souhail <ysouhail@c1r3p7.1337.ma>
2025-07-30 12:10:37 +0100

    done

--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -23,17 +23,23 @@ class PmergeMe
 		std::string trim(const std::string &str);
 		// void vec_sort();
 		//sort for the vector
+		// template <typename Container>
+		void addToWinners(std::vector<int>& winner, const std::vector<int>& losers, int odd);
 		void sort_fn_vec();
 		std::vector<int> vec_sort(std::vector<int> vect);
-		void addToWinners(std::vector<int>& winner, const std::vector<int>& losers, int odd);
-		template <typename Container>
-		void binaryInsert(Container& cont, int value);
+		void binaryInsert(std::vector<int>& cont, int value);
 		std::vector<size_t> getJacobsthalIndices(size_t size);
-		// sort for vector
-		void  sort_fn_deq();
-		void deque_sort();
-		/// use
 		void fillPair(std::vector<std::pair<int, int> >& pairs, int& odd, std::vector<int> vect);
 		void SplitNumbers(std::vector<std::pair<int, int> >& pairs, std::vector<int>& winner, std::vector<int>& loser);
+		// sort for vector
+		// deque **
+		void  sort_fn_deq();
+		std::deque<int> deque_sort(std::deque<int> deque);
+		void fillPairDeque(std::deque<std::pair<int, int> >& pairs, int& odd, std::deque<int> vect);
+		void SplitNumbersDeque(std::deque<std::pair<int, int> >& pairs, std::deque<int>& winner, std::deque<int>& loser);
+		void addToWinnersDeque(std::deque<int>& winner, const std::deque<int>& losers, int odd);
+		std::deque<size_t> getJacobsthalIndicesDeque(size_t size);
+		void binaryInsertDeque(std::deque<int>& cont, int value);
+		/// use
 		~PmergeMe();
 };
